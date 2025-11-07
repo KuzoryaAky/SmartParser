c0ba97943c1c87472518cb4c41a594948894d607
KuzoryaAky <opanforever1992@gmail.com>
2025-11-07 21:33:41 +0300

    update ParserService

--- SmartParser.Console/Services/ParserService.cs
+++ SmartParser.Console/Services/ParserService.cs
@@ -1,6 +1,23 @@
-﻿namespace SmartParser.Console.Models;
+﻿using System.Text.Json;
+
+namespace SmartParser.Console.Service;
 
 public class ParserService
 {
-  
+    public async Task<List<NewsItem>> ParseNewsAsync()
+    {
+        try
+        {
+            using var client = new HttpClient();
+            var response = await client.GetStringAsync("https://jsonplaceholder.typicode.com/posts");
+
+            var news = JsonSerializer.Deserialize<List<NewsItem>>(response);
+            return news ?? new List<NewsItem>();
+        }
+        catch (Exception ex)
+        {
+            System.Console.WriteLine($"Error parsing news: {ex.Message}");
+            throw;
+        }
+    }
 }
